fd5fa7734958604ecaa42b9ea7c735971754df15
Sebastian Yeo <khyeo81@gmail.com>
2025-10-18 16:46:03 -0700

    Initial UI

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import ForgeReconciler, { Button, ButtonGroup, Text, Textfield } from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+  const [input, setInput] = useState(null);
+
+  const changeText = async () => {
+    
+  };
+
+  const InputButtons = () => (
+    <ButtonGroup>
+      <VoiceInput />
+      <SubmitButton />
+    </ButtonGroup>
+  );
+  
+  const getVoiceInput = async () => {
+    const result = await invoke('getText', { example: 'my-invoke-variable' });
+    setInput(result);
+  };
+
+  const VoiceInput = () => (
+    <Button onClick={getVoiceInput}>Voice Input</Button>
+  );
+  
+  const SubmitButton = () => (
+    <Button appearance='primary' onClick={changeText}>Submit</Button>
+  );
+
   return (
     <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      <Textfield id = "inputfield" placeholder={input}></Textfield>
+      <InputButtons/>
     </>
   );
 };
